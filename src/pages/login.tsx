b4004c8dedf4adab01b1c6401a927abb30224448
phuc.pham3 <phuc.pham3>
2023-03-16 16:55:55 +0700

    use img tag

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,4 @@
 import { signIn, signOut, useSession } from "next-auth/react";
-import Image from "next/image";
 
 export default function Login() {
   const { data: session } = useSession();
@@ -8,20 +7,20 @@ export default function Login() {
     console.log("session", session);
     return (
       <div className="cursor-pointer" onClick={() => signOut()}>
-        <Image
-          src="https://t4.ftcdn.net/jpg/00/62/89/77/360_F_62897778_Mpe0AmVF2IdNaAz7eNIW1vvpJQ7gSGne.jpg"
-          alt="Picture of the author"
+        <img
           width={80}
+          src="https://t4.ftcdn.net/jpg/00/62/89/77/360_F_62897778_Mpe0AmVF2IdNaAz7eNIW1vvpJQ7gSGne.jpg"
+          alt="google-logout"
         />
       </div>
     );
   }
   return (
     <div onClick={() => signIn()} className="cursor-pointer">
-      <Image
-        src="https://onymos.com/wp-content/uploads/2020/10/google-signin-button-1024x260.png"
-        alt="Picture of the author"
+      <img
         width={200}
+        src="https://onymos.com/wp-content/uploads/2020/10/google-signin-button-1024x260.png"
+        alt="google-login"
       />
     </div>
   );
